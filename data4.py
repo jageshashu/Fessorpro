6036ae03e3a6b26fc2f77587830b8e8840409687
jageshashu <sahuashish34@gmail.com>
2026-08-23 16:15:36 +0530

    Aug_28

---
+++ data4.py
@@ -0,0 +1,13 @@
+
+
+# string 2 new function 
+# Split
+# join
+
+s1='nifty,banknifty,tsla,apple,microsoft'
+l1=s1.split(',')
+print(l1)
+
+l1=['nifty', 'banknifty', 'tsla', 'apple', 'microsoft']
+s2=','.join(l1)
+print(s2)
