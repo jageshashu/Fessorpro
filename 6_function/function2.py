dcef73981c8f35a3e29415f7f304f9e02059fd3c
jageshashu <sahuashish34@gmail.com>
2026-09-11 09:05:13 +0530

    Ash

--- 6_function/function2.py
+++ 6_function/function2.py
@@ -9,7 +9,7 @@ def is_odd(num:int)->bool:
     else:
         return True
 
-    def is_even(num:int)->bool:
+def is_even(num:int)->bool:
         if num%2==0:
             return True
         else:
@@ -19,3 +19,16 @@ def is_odd(num:int)->bool:
 num=10
 print(is_odd(num))
 print(is_even(num))
+
+def reverse(string1:str)->str:
+     rev_str=''
+     for i in string1:
+          rev_str=i+rev_str
+     return rev_str
+
+def is_palendrome(string1:str)->bool:
+     rev_str=reverse(string1)
+     if string1==rev_str:
+          return True
+     else:
+          return False     
